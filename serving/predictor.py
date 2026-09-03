9a921581f75cff527543270e838faab6bd2529c2
amirhossein <amir0hossin0pr@gmail.com>
2026-09-03 14:27:40 +0330

    implement predictor

---
+++ serving/predictor.py
@@ -0,0 +1,18 @@
+import torch 
+from model.model import SimpleModel
+
+
+class Predictor:
+    def __init__(self,model:SimpleModel):
+        self.model = model
+
+    def predict(self,inputs:list[list[float]]) -> list[float]:
+        tensor = torch.tensor(
+            inputs,
+            dtype=torch.float32
+        )
+
+        with torch.inference_model():
+            predictions = self.model(tensor)
+        
+        return predictions.squeeze(-1).tolist()
