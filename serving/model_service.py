b1dc55c9dea3c2a5d81fcd66f292874c43cb7ba7
amirhossein <amir0hossin0pr@gmail.com>
2026-09-03 14:43:49 +0330

    add property for check status

--- serving/model_service.py
+++ serving/model_service.py
@@ -9,14 +9,21 @@ class ModelService:
 
         self._predictor:Predictor|None = None
 
+    @property
+    def is_ready(self)-> bool:
+        return self._predictor is not None
+
     def start(self):
+        if self.is_ready:
+            return
+        
         loader = ModelLoader(self.model_path)
 
         model = loader.load()
         self._predictor = Predictor(model)
 
     def predict(self,inputs:list[list[float]]) -> list[float]:
-        if self._predictor is None:
+        if not self.is_ready:
             raise RuntimeError(
                 "Modle service has not been started. "
             )
