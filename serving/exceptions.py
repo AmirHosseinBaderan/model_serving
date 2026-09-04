eafef8caf509964320f8feac23c4dbd1b75f5a58
amirhossein <amir0hossin0pr@gmail.com>
2026-09-04 23:44:22 +0330

    Update exceptions.py

--- serving/exceptions.py
+++ serving/exceptions.py
@@ -1,2 +1,6 @@
 class ModelNotReadyError(RuntimeError):
     """Raised when inference is requested before the model is ready."""
+
+
+class InferenceError(RuntimeError):
+    """Raised when model inference fails."""
