61341ad2f738aeffb93f1a2d4c186b32bb833178
amirhossein <amir0hossin0pr@gmail.com>
2026-09-05 23:53:34 +0330

    update : change configs

--- serving/config.py
+++ serving/config.py
@@ -3,8 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class AppConfig(BaseSettings):
     model_backend: str = "onnx"
-    model_path: str
-
+    model_name: str
+    model_version: str
+    model_artifacts_root: str
+    
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
