74d91143b7c8af0fbe27601b85ab2f07acf1e04b
amirhossein <amir0hossin0pr@gmail.com>
2026-09-06 23:34:23 +0330

    update : add run function
    - update run status

--- tracking/in_memory_tracker.py
+++ tracking/in_memory_tracker.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-from typing import Any
 from uuid import uuid4
 
-from .run import Run
+from .run import Run,RunStatus
 from .tracker import ExperimentTracker
 
 class InMemoryExperimentTracker(ExperimentTracker):
@@ -21,10 +20,30 @@ class InMemoryExperimentTracker(ExperimentTracker):
         return run
     
     def log_parameter(self, run, name, value):
+        run.ensure_active()
+        
         run.parameters[name] = value
         
     def log_metric(self, run, name, value):
+        run.ensure_active()
+        
         if name not in run.metrics:
             run.metrics[name] = []
             
         run.metrics[name].append(value)
+        
+    def finish_run(
+        self,
+        run: Run,
+    ) -> None:
+        run.ensure_active()
+        
+        run.status = RunStatus.COMPLETED
+
+    def fail_run(
+        self,
+        run: Run,
+    ) -> None:
+        run.ensure_active()
+        
+        run.status = RunStatus.FAILED
